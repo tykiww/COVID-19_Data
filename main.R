8bba6e4cb5e20bd54c036a5a04905ef5b5ce71a7
Tyki Wada <36966666+tykiww@users.noreply.github.com>
2020-03-22 19:11:19 -0600

    Create main.R

---
+++ main.R
@@ -0,0 +1,47 @@
+# For script
+
+# Initialize
+config <- config::get(file="config.yml")
+source(utilities.R)
+
+
+# main
+
+main <- function(destination) {
+  
+  # Retrieve Request
+  raw = common$scraper(config$folder_url,
+                       config$xml_tag)
+  # Extract csv names
+  urls = common$extractor(raw,config$base_url)
+  
+  # Retrieve Tables
+  dfs = lapply(urls, function(x) dplyr::select(readr::read_csv(x), config$columns))
+  
+  # Check table for date format
+  idds = sapply(1:length(urls),function(x) common$checker(x,dfs))
+  
+  # Format Dates
+  insert = lapply(1:sum(idds),function(x) common$manipulator(dfs[idds][[x]]))
+  dfs[idds] = insert
+  
+  # Reformat for output and clean
+  final_df = do.call(rbind,dfs)
+  common$cleaner(c("final_df","common"))
+  
+  return(final_df)
+}
+
+
+
+# If scripting, it will write a csv. otherwise this can be used as a source() file
+if (!interactive()) {
+  write.csv(main(),"data/covid_data.csv")
+} else {
+  df = main()
+}
+
+
+
+
+
